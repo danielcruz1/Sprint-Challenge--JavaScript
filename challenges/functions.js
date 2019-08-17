5b2c7f4eab5d0002bfc70539580b0e6266ea7124
staccats <47672588+staccats@users.noreply.github.com>
2019-08-16 21:25:45 -0600

    Sprint Challenge - JavaScript: Complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,27 +6,31 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+consume = (x, y, cb) => {return cb(x, y)}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+add = (x, y) => {return x + y}
 
+multiply = (x, y) => {return x * y}
+
+greeting = (firstName, lastName) => {return `Hello ${firstName} ${lastName}, nice to meet you!`}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
-
+// Explanation: 'nestedfunction()' can access the variable 'internal' becuase it is part of the inner most function. Since 'internal'
+//               is not found in it's own scope, the 'nestedfunction()' will expand it's search to the scope of whatever function it is nested in.  
 
 const external = "I'm outside the function";
 
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,27 +8,55 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+const trex = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous'
+}
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const steg = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+}
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+const raptor = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous'
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(trex.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(raptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(steg.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(trex.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+const letRoar = () => {
+  return 'RAWERSRARARWERSARARARRRR!';
+}
+trex.roar = letRoar;
+console.log(trex.roar());
 
 
 // ==== Arrays ====
@@ -50,7 +78,10 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+for(let i = 0; i < graduates.length; i++) {
+  universities.push(graduates[i].university);
+}
+console.log(universities.sort())
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -59,11 +90,19 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for(let i = 0; i < graduates.length; i++) {
+  contactInfo.push(graduates[i].first_name + ' ' + graduates[i].email);
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for(let i = 0; i < graduates.length; i++) {
+  if (graduates[i].university.includes('Uni')) {
+    uni.push(graduates[i].university);
+  }
+}
 console.log(uni);
 
 
@@ -89,31 +128,48 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+const name = zooAnimals.forEach((object) => {
+  let name = 'Name:' + ' ' + object.animal_name + ', ' + 'Scientific:' + ' ' + object.scientific_name + '.';
+  animalNames.push(name);
+})
 console.log(animalNames);
+
 
 /* Request 2: .map()    
 
 The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 
 */
-
 const lowerCase = [];
-console.log(lowerCase); 
+  smallName = zooAnimals.map(item => item.animal_name.toLowerCase());
+console.log(smallName); 
 
 /* Request 3: .filter() 
 
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const lowerPopulation = [];
-console.log(lowerPopulation);
+let lowerPopulation = [];
+let popFive = (object) => {
+  return object.population < 5;
+}
+lowerPopulation = zooAnimals.filter(popFive);
+console.log(lowerPopulation)
+
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+let populationTotal = [];
+let pop = (x, y) => {
+  return x + y;
+}
+zooAnimals.forEach((object) => {
+  populationTotal.push(object.population)
+})
+populationTotal = populationTotal.reduce(pop);
 console.log(populationTotal);
 
 
